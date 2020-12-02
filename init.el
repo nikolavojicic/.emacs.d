7ec86fe54570b9fe9fa0c9c8d61497854632e38d
nikolavojicic <nikolavojicic@outlook.com>
2020-12-02 13:13:31 +0100

    Fix indent

--- init.el
+++ init.el
@@ -2,9 +2,9 @@
 
 
 (setq package-archives
-      '(("gnu"       . "https://elpa.gnu.org/packages/")
-        ;("melpa"     . "https://melpa.org/packages/")
-        ;("marmalade" . "https://marmalade-repo.org/packages/")
+      '(("gnu"          . "https://elpa.gnu.org/packages/")
+      ;;("melpa"        . "https://melpa.org/packages/")
+      ;;("marmalade"    . "https://marmalade-repo.org/packages/")
         ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
 
