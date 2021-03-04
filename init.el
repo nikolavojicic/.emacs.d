458859ec0db835781fdbaf825339586180573b04
nikolavojicic <nikolavojicic@outlook.com>
2021-03-04 15:01:05 +0100

    Remove beacon mode as it doesn't work well with line numbers

--- init.el
+++ init.el
@@ -18,7 +18,6 @@
 (dolist (package '(smex
                    cider
                    magit
-                   beacon
                    company
                    flx-ido
                    paredit
@@ -58,7 +57,6 @@
 ;;  \__,_|_|
 
 
-(beacon-mode             1)
 (menu-bar-mode          -1)
 (tool-bar-mode          -1)
 (scroll-bar-mode        -1)
