54595e89dc439458727d2d1a7600ee418609edeb
nikolavojicic <nikolavojicic@outlook.com>
2021-06-07 16:59:02 +0200

    Add avy mode

--- init.el
+++ init.el
@@ -15,7 +15,8 @@
   (package-refresh-contents))
 
 
-(dolist (package '(smex
+(dolist (package '(avy
+                   smex
                    cider
                    magit
                    company
@@ -185,6 +186,7 @@
 
 (global-set-key (kbd "M-x")     #'smex)
 (global-set-key (kbd "C-x C-b") #'ibuffer)
+(global-set-key (kbd "C-'")     #'avy-goto-char-timer)
 
 
 (with-eval-after-load 'org
