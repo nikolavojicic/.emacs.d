5f22222d0ba0c4c661c6eb9e1e7eff546ec64607
Nikola Vojicić <nikola.vojicic@belit.co.rs>
2023-05-26 12:54:42 +0200

    Make comment face bold and italic

--- concrete-theme.el
+++ concrete-theme.el
@@ -10,6 +10,7 @@
  '(mode-line-buffer-id ((t (:foreground "blue"))))
  '(line-number-current-line ((t (:foreground "red"))))
  '(font-lock-doc-face ((t (:foreground "firebrick" :slant italic))))
+ '(font-lock-comment-face ((t (:foreground "firebrick" :weight bold :slant italic))))
  '(magit-section-highlight ((t (:background "gray90"))))
  '(magit-diff-context-highlight ((t (:background "gray90"))))
  '(org-block ((t (:background "gray90"))))
