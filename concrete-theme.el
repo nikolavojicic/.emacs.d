5607bfd9f7f132ee557f3680d4ff3adae4e4820e
Nikola Vojicić <nikola.vojicic@belit.co.rs>
2023-07-26 15:56:16 +0200

    Gray comments

--- concrete-theme.el
+++ concrete-theme.el
@@ -10,7 +10,7 @@
  '(mode-line-buffer-id ((t (:foreground "blue"))))
  '(line-number-current-line ((t (:foreground "red"))))
  '(font-lock-doc-face ((t (:foreground "firebrick" :slant italic))))
- '(font-lock-comment-face ((t (:foreground "firebrick" :weight bold))))
+ '(font-lock-comment-face ((t (:foreground "gray40"))))
  '(magit-section-highlight ((t (:background "gray90"))))
  '(magit-diff-context-highlight ((t (:background "gray90"))))
  '(org-block ((t (:background "gray90"))))
